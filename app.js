29b93b3a7971bafd291e5d07d77fb12c46f002cd
RaidenOM <omkumars2005@gmail.com>
2025-04-05 12:35:22 +0530

    updated

--- app.js
+++ app.js
@@ -52,7 +52,18 @@ app.get("/profile", verifyToken, async (req, res) => {
 });
 
 async function generatePdf(html) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
   const page = await browser.newPage();
   await page.setContent(html);
   const pdfBuffer = await page.pdf({
